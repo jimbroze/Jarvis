d8daf0ad12b112c20a0c9d4b1d5ec7a4acbcc0ef
jimbroze <james.n.dickinson@gmail.com>
2021-10-20 20:57:38 +0100

    Test fan

--- Docker/appdaemon/apps/gpio.py
+++ Docker/appdaemon/apps/gpio.py
@@ -10,7 +10,15 @@ class ServerFan(hass.Hass):
         GPIO.setup(self.fanPin, GPIO.OUT, initial=GPIO.LOW)
         # 55 and 45
 
+        self.listen_state(self.change_fan_state, "sensor.processor_temperature")
+
+    def temp_callback(self, entity, attribute, old, new, kwargs):
+        if old < 55 and new > 55:
+            self.change_fan_state(True)
+        elif old > 45 and new < 45:
+            self.change_fan_state(False)
+
     def change_fan_state(self, state=False):
         gpioState = {False: GPIO.LOW, True: GPIO.HIGH}[state]
         GPIO.output(self.fanPin, gpioState)
-        fanState = GPIO.input(self.fanPin)               
+        fanState = GPIO.input(self.fanPin)              
