448e8c67e0d3c0ab540d7bc551f3dc63a46198e1
jimbroze <james.n.dickinson@gmail.com>
2021-10-20 17:54:59 +0100

    Created gpio appdaemon

---
+++ Docker/appdaemon/apps/gpio.py
@@ -0,0 +1,17 @@
+import R64.GPIO as GPIO
+
+import hassapi as hass
+
+
+class ServerFan(hass.Hass):
+    def initialize(self):
+        self.fanPin = 16
+        GPIO.setwarnings(True)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.fanPin, GPIO.OUT, initial=GPIO.LOW)
+        # 55 and 45
+
+    def change_fan_state(self, state=False):
+        gpioState = {False: GPIO.LOW, True: GPIO.HIGH}[state]
+        GPIO.output(self.fanPin, gpioState)
+        fanState = GPIO.input(self.fanPin)               
--- Docker/appdaemon/apps/notifications/notifications.py
+++ Docker/appdaemon/apps/notifications/notifications.py
@@ -1,7 +1,7 @@
-import random
-import time
+# import random
+from datetime import datetime
 
-from persistqueue import Queue
+from persistqueue import SQLiteAckQueue
 
 # import globals
 import hassapi as hass
@@ -10,34 +10,126 @@ import mqttapi as mqtt
 
 class Notifications(hass.Hass):
     def initialize(self):
-        # Initialize callbacks
-        self.q = Queue("mypath")
+        self.qPath = "notification_q"
+        self.types = self.args["types"]
+        self.defaults = self.args["default_destinations"]
+        self.tts = self.get_app("sound")
+        self.qSizeLocation = "input_number.queue_size"
+        self.someoneHomeLocation = self.entities.input_boolean.someone_home.state
 
-    def new_notification(self, type, message):
+        self.listen_state(self.get_notifications, "binary_sensor.someone_home")
+
+    # ----------- Helper functions -----------
+
+    # self.turn_on("entity.name")
+
+    def someone_home(self):
+        return self.someoneHomeLocation
+
+    # ----------- Main functions -----------
+
+    def new_notification(self, notificationType, message):
         """Raise a new notification with a given 'type' and 'message'"""
-        urgency = self.check_urgency(type)
+        urgency = self.types[notificationType]["urgency"]
+        destination = self.types[notificationType].get(
+            "destination", self.defaults[urgency]
+        )
+
         notifications = 0
-        if urgency in ("Immediate", "Both"):
-            self.notify_urgent(message)
-            notifications += 1
-        if urgency in ("Queued", "Both"):
-            self.notify_not_urgent(message, urgency)
-            notifications += 1
+
+        if self.someone_home():
+            if urgency in ("Immediate", "Both", "Queued", "If_Home"):
+                self.send_notification(message, destination, title=notificationType)
+                notifications += 1
+        else:
+            if urgency in ("Immediate", "Both"):
+                self.send_notification(message, destination, title=notificationType)
+                notifications += 1
+            if urgency in ("Queued", "Both"):
+                # Store notification in queue for later
+                self.queue_notification(message, destination, notificationType)
+                notifications += 1
+
         if notifications == 0:
             self.log(f"'{urgency}' is not a valid urgency.", level="ERROR")
 
-    def check_urgency(self, type):
-        """Check whether nofication can be queued"""
-        return self.urgencies[type]
+    def send_notification(
+        self, message, destination, notificationType, title="", time=""
+    ):
+        if destination.split(".")[0] == "media_player":
+            try:
+                self.tts.notify(
+                    message,
+                    notificationType,
+                    destination,
+                    time,
+                )
+            except:
+                self.log(f"Error sending notification.", level="ERROR")
+                return False
+        else:
+            try:
+                self.notify(message, title=title, name=destination)
+            except:
+                self.log(f"Error sending notification.", level="ERROR")
+                return False
+        return True
 
-    def notify_not_urgent(self, message, urgency):
-        """Check if home"""
-        self.notify_later(message, urgency)
+    def queue_notification(self, message, destination, notificationType):
+        q = SQLiteAckQueue(self.qPath)
+        q.put(
+            {
+                "message": message,
+                "destination": destination,
+                "notificationType": notificationType,
+                "time": datetime.now(),
+            }
+        )
+        # Update helper to show number of notifications waiting. active_size includes ack cache
+        self.set_value(self.qSizeLocation, self.q.active_size())
 
-    def notify_later(self):
-        self.q.put("a")
-        self.q.get()
-        self.q.task_done()  # Remove from q
+    def get_notifications(self, entity, attribute, old, new, kwargs):
+        if new == False:  # Only continue if someone is home
+            return
+        q = SQLiteAckQueue(self.qPath)
+
+        # Make list of speech notes and publish together
+        voiceNotes = []
+        while q.qsize > 0:  # qSize reduces with get()
+            notification = q.get()
+
+            if notification["time"].day == datetime.today().day:
+                # Time only
+                title = (
+                    notification["notificationType"]
+                    + "from "
+                    + notification["time"].time()
+                )
+            else:  # Include date
+                title = (
+                    notification["notificationType"] + "from " + notification["time"]
+                )
+
+            if notification["destination"].split(".")[0] == "media_player":
+                voiceNotes.append(notification)
+            # Others are sent individually
+            elif self.send_notification(
+                notification["message"],
+                notification["destination"],
+                notification["notificationType"],
+                title,
+                notification["time"],
+            ):
+                self.q.ack(notification)
+            else:
+                self.q.nack(notification)
+                self.log(f"Notifications not delivered", level="ERROR")
+
+        if self.tts.multi_notify(voiceNotes):
+            [q.ack(item) for item in voiceNotes]
+        else:
+            [q.nack(item) for item in voiceNotes]
+            self.log(f"Notifications not delivered", level="ERROR")
 
 
 class NotificationListener(mqtt.Mqtt):
